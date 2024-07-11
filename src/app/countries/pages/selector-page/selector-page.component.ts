4e5236cf46a63d62685e17cd00eae48eb60a692d
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-11 11:37:27 -0600

    feat: First Selector

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-selector-page',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class SelectorPageComponent {
+public myForm:FormGroup = this.fb.group({
+  region : [ '', Validators.required ],
+  country: [ '', Validators.required ],
+  borders: [ '', Validators.required ],
+})
+
+  constructor(
+    private fb:FormBuilder
+  ){}
 
 }
