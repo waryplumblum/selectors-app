40928b2bd60799faa395251df717c72b595ba8b5
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-11 11:46:58 -0600

    feat: countries service and country interface

---
+++ src/app/countries/interfaces/country.interfaces.ts
@@ -0,0 +1,155 @@
+export enum Region {
+  Africa  = 'Africa' ,
+  America = 'America',
+  Asia    = 'Asia'   ,
+  Europa  = 'Europa' ,
+  Oceania = 'Oceania',
+}
+
+export interface TopLevel {
+  name:         Name;
+  cca3:         string;
+  status:       Status;
+  idd:          Idd;
+  capital:      string[];
+  altSpellings: string[];
+  region:       string;
+  subregion:    Subregion;
+  languages:    { [key: string]: string };
+  translations: { [key: string]: Translation };
+  latlng:       number[];
+  landlocked:   boolean;
+  borders?:     string[];
+  area:         number;
+  demonyms:     Demonyms;
+  flag:         string;
+  maps:         Maps;
+  population:   number;
+  fifa?:        string;
+  car:          Car;
+  timezones:    string[];
+  continents:   string[];
+  flags:        Flags;
+  coatOfArms:   CoatOfArms;
+  startOfWeek:  StartOfWeek;
+  capitalInfo:  CapitalInfo;
+  cioc?:        string;
+  gini?:        { [key: string]: number };
+  postalCode?:  PostalCode;
+}
+
+export interface CapitalInfo {
+  latlng: number[];
+}
+
+export interface Car {
+  signs: string[];
+  side:  Side;
+}
+
+export enum Side {
+  Left = "left",
+  Right = "right",
+}
+
+export interface CoatOfArms {
+  png?: string;
+  svg?: string;
+}
+
+export interface Currencies {
+  GIP?: All;
+  ISK?: All;
+  EUR?: All;
+  GBP?: All;
+  GGP?: All;
+  BYN?: All;
+  ALL?: All;
+  NOK?: All;
+  UAH?: All;
+  PLN?: All;
+  RUB?: All;
+  DKK?: All;
+  FOK?: All;
+  CZK?: All;
+  JEP?: All;
+  SEK?: All;
+  MKD?: All;
+  IMP?: All;
+  MDL?: All;
+  RSD?: All;
+  RON?: All;
+  BAM?: BAM;
+  HUF?: All;
+  BGN?: All;
+  CHF?: All;
+}
+
+export interface All {
+  name:   string;
+  symbol: string;
+}
+
+export interface BAM {
+  name: string;
+}
+
+export interface Demonyms {
+  eng:  Eng;
+  fra?: Eng;
+}
+
+export interface Eng {
+  f: string;
+  m: string;
+}
+
+export interface Flags {
+  png:  string;
+  svg:  string;
+  alt?: string;
+}
+
+export interface Idd {
+  root:     string;
+  suffixes: string[];
+}
+
+export interface Maps {
+  googleMaps:     string;
+  openStreetMaps: string;
+}
+
+export interface Name {
+  common:     string;
+  official:   string;
+  nativeName: { [key: string]: Translation };
+}
+
+export interface Translation {
+  official: string;
+  common:   string;
+}
+
+export interface PostalCode {
+  format: string;
+  regex:  string;
+}
+
+export enum StartOfWeek {
+  Monday = "monday",
+}
+
+export enum Status {
+  OfficiallyAssigned = "officially-assigned",
+  UserAssigned = "user-assigned",
+}
+
+export enum Subregion {
+  CentralEurope = "Central Europe",
+  EasternEurope = "Eastern Europe",
+  NorthernEurope = "Northern Europe",
+  SoutheastEurope = "Southeast Europe",
+  SouthernEurope = "Southern Europe",
+  WesternEurope = "Western Europe",
+}
