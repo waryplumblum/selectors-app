12733553d2d23a82cebc48f67c0a241de6aa353e
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-11 11:26:28 -0600

    feat: LazyLoad

---
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selector-page',
+  templateUrl: './selector-page.component.html',
+  styles: ``
+})
+export class SelectorPageComponent {
+
+}
